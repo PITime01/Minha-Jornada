10000e487bfe578817fe864eff627b4664dc3755
Carolina M <carolinamargiott2i@gmail.com>
2020-11-07 14:26:07 -0300

    feito CRUD dos veiculos

---
+++ codigo-fonte/src/main/java/com/example/PITime01/controllers/VehicleController.java
@@ -0,0 +1,62 @@
+package com.example.PITime01.controllers;
+
+import com.example.PITime01.employee.Employee;
+import com.example.PITime01.vehicle.Vehicle;
+import com.example.PITime01.vehicle.VehicleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+public class VehicleController {
+    @Autowired
+    private VehicleService service;
+    private final String viewFolder = "vehicle/";
+
+    @RequestMapping("/vehicle/list")
+    public String viewVehicleHomePage(Model model){
+        System.out.println("b");
+        List<Vehicle> listVehicles=service.listAll();
+        if (listVehicles != null) {
+            model.addAttribute("listVehicles",listVehicles);
+        }
+        return viewFolder+"list";
+    }
+
+    @RequestMapping("/vehicle/new")
+    public String ShowNewVehicleForm(Model model){
+        Vehicle vehicle=new Vehicle();
+        model.addAttribute("vehicle",vehicle);
+
+        return viewFolder+"new";
+    }
+
+    @RequestMapping(value ="vehicle/save", method= RequestMethod.POST)
+    public String saveVehicle(@ModelAttribute("vehicle") Vehicle vehicle){
+        service.Save(vehicle);
+        return "redirect:/vehicle/list";
+    }
+
+    @RequestMapping("/vehicle/edit/{id}")
+    public ModelAndView ShowEditVehicleForm(@PathVariable(name="id") Long id){
+        System.out.println("a");
+        ModelAndView mav=new ModelAndView("vehicle/edit");
+        Vehicle vehicle=service.get(id);
+        mav.addObject("vehicle",vehicle);
+        return mav;
+    }
+
+    @RequestMapping("/vehicle/delete/{id}")
+    public String deleteProduct(@PathVariable(name="id") Long id){
+        System.out.println("puf");
+        service.delete(id);
+        return "redirect:/vehicle/list";
+    }
+}
---
+++ codigo-fonte/src/main/java/com/example/PITime01/vehicle/VehicleRepository.java
@@ -0,0 +1,7 @@
+package com.example.PITime01.vehicle;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
+
+}
