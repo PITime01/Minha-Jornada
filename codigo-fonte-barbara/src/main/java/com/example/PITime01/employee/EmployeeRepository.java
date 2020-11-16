ef77e9d9f00bc33b7ad6967ebd7ee44dd997af60
Barbara-BB <60778277+Barbara-BB@users.noreply.github.com>
2020-11-16 10:47:05 -0300

    Delete EmployeeRepository.java

--- codigo-fonte-barbara/src/main/java/com/example/PITime01/employee/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.PITime01.employee;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface EmployeeRepository extends JpaRepository<Employee,Long> {
-    Optional<Employee> findByName(String name);
-}
