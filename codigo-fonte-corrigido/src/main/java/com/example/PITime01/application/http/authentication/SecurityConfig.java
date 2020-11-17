e4d8475ebcfcc163633a36f82e813930ca3acacb
Barbara-BB <60778277+Barbara-BB@users.noreply.github.com>
2020-11-16 21:55:57 -0300

    Add files via upload

---
+++ codigo-fonte-corrigido/src/main/java/com/example/PITime01/application/http/authentication/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.example.PITime01.application.http.authentication;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import static com.example.PITime01.domain.Profile.ADMIN;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    final
+    MyUserDetailService userDetailsService;
+
+    public SecurityConfig(MyUserDetailService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .and().formLogin()
+                .loginPage("/login")
+                .permitAll();
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){ return new BCryptPasswordEncoder(); }
+
+}
